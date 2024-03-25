306aeb7e22360c6881c3636517c0c08ddb9d4d22
contradiction29 <sora32127@gmail.com>
2024-03-25 13:53:59 +0900

    Import Noto Sans

--- app/root.tsx
+++ app/root.tsx
@@ -11,6 +11,12 @@ import stylesheet from "~/tailwind.css?url";
 
 export const links: LinksFunction = () => [
   { rel: "stylesheet", href: stylesheet },
+  { rel: 'preconnect', href: 'https://fonts.googleapis.com' },
+  { rel: 'preconnect', href: 'https://fonts.gstatic.com', crossOrigin: 'anonymous' },
+  {
+    rel: 'stylesheet',
+    href: 'https://fonts.googleapis.com/css2?family=Noto+Sans+JP:wght@100..900&family=Ubuntu:ital,wght@0,300;0,400;0,500;0,700;1,300;1,400;1,500;1,700&display=swap',
+  },
 ];
 
 
