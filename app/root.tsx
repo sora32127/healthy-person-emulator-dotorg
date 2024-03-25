4f651770157c78aa5c7439f84c3b025632561e1d
contradiction29 <sora32127@gmail.com>
2024-03-25 13:47:23 +0900

    Initialize tailwind

--- app/root.tsx
+++ app/root.tsx
@@ -6,6 +6,14 @@ import {
   ScrollRestoration,
 } from "@remix-run/react";
 
+import type { LinksFunction } from "@remix-run/node";
+import stylesheet from "~/tailwind.css?url";
+
+export const links: LinksFunction = () => [
+  { rel: "stylesheet", href: stylesheet },
+];
+
+
 export function Layout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
---
+++ tailwind.config.ts
@@ -0,0 +1,10 @@
+import type { Config } from 'tailwindcss'
+
+export default {
+  content: ['./app/**/*.tsx'],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+} satisfies Config
+
