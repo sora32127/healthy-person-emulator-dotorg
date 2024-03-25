60e4c4969b0b77e103c470fe19f20c843fcb6a81
contradiction29 <sora32127@gmail.com>
2024-03-25 15:25:01 +0900

    Add playwright automatic axe test

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -1,4 +1,5 @@
 import { test, expect } from '@playwright/test';
+import AxeBuilder from '@axe-core/playwright';
 
 const localHostUrl = 'http://localhost:5173';
 
@@ -12,4 +13,24 @@ test('get feed link', async ({ page }) => {
   await page.getByRole('link', { name: 'Go to the feed' }).click();
   await page.getByRole('link', { name: 'パソコンのキーボードはフロントUSBに刺したほうがいい' }).click();
   await expect(page).toHaveTitle(/パソコンのキーボードはフロントUSBに刺したほうがいい/);
+});
+
+test.describe('homepage', () => {
+  test('Auto axe test in TopPage', async ({ page }) => {
+    await page.goto(localHostUrl);
+
+    const accessibilityScanResults = await new AxeBuilder({ page }).analyze();
+
+    expect(accessibilityScanResults.violations).toEqual([]);
+  });
+
+  test('Auto axe test in FeedPage', async ({ page }) => {
+    await page.goto(`${localHostUrl}/feed`);
+
+    const accessibilityScanResults = await new AxeBuilder({ page }).analyze();
+
+    expect(accessibilityScanResults.violations).toEqual([]);
+  });
+
+
 });
