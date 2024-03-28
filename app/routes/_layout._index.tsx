620365a1246d485a1565dbd9afa4a6e581fb4432
contradiction29 <sora32127@gmail.com>
2024-03-28 19:32:51 +0900

    Implement feed search by vote

--- app/routes/_layout._index.tsx
+++ app/routes/_layout._index.tsx
@@ -118,6 +118,7 @@ export default function Feed() {
                 countDislikes={post.countDislikes}
             />
         ))}
+        <NavLink to="/feed?p=2&likeFrom=24&likeTo=0">次へ</NavLink>
         </>
 
     );
--- app/routes/_layout.feed.tsx
+++ app/routes/_layout.feed.tsx
@@ -1,64 +1,162 @@
 import { LoaderFunctionArgs, json, redirect, ActionFunctionArgs } from "@remix-run/node";
-import { useLoaderData, useSearchParams, Form } from "@remix-run/react";
+import { useLoaderData, Form } from "@remix-run/react";
 import { prisma } from "~/modules/db.server";
 import PostCard from "~/components/PostCard";
 
 export async function loader({ request }: LoaderFunctionArgs) {
   const url = new URL(request.url);
   const pagingNumber = parseInt(url.searchParams.get("p") || "1");
+  const likeFromHour = url.searchParams.get("likeFrom");
+  const likeToHour = url.searchParams.get("likeTo");
+  let postData = [];
 
-  const mostRecentPosts = await prisma.userPostContent.findMany({
-    orderBy: { postDateJst: "desc" },
-    skip: (pagingNumber - 1) * 10,
-    take: 10,
-    select: {
-      postId: true,
-      postTitle: true,
-      postDateJst: true,
-      postUrl: true,
-      countLikes: true,
-      countDislikes: true,
-    },
-  });
+  if (likeFromHour) {
+    const likeFromHourInt = parseInt(likeFromHour);
+    const likeToHourInt = parseInt(likeToHour || "0");
+    const now = new Date();
+    const gteDate = new Date(now.getTime() - likeFromHourInt * 60 * 60 * 1000);
+    const lteDate = new Date(now.getTime() - likeToHourInt * 60 * 60 * 1000);
 
-  const allTagNames = await prisma.dimTags.findMany({
-    select: {
-      postId: true,
-      tagName: true,
-    },
-    where: { postId: { in: mostRecentPosts.map((post) => post.postId) } },
-  });
+    const recentVotedPostsAgg = await prisma.fctPostVoteHisotry.groupBy({
+        by: ["postId"],
+        where: {
+            voteDateGmt: {
+            gte: gteDate,
+            lte: lteDate,
+            },
+            voteTypeInt: { in: [1] },
+        },
+        _count: { voteUserIpHash: true },
+        orderBy: { _count: { voteUserIpHash: "desc" } },
+        take: 10,
+        skip: (pagingNumber - 1) * 10,
+    });
 
-  const postData = mostRecentPosts.map((post) => {
-    const tagNames = allTagNames
-      .filter((tag) => tag.postId === post.postId)
-      .map((tag) => tag.tagName);
-    return { ...post, tagNames };
-  });
+    const recentVotedPosts = await prisma.userPostContent.findMany({
+        where: { postId: { in: recentVotedPostsAgg.map((post) => post.postId) } },
+        select: {
+            postId: true,
+            postTitle: true,
+            postDateJst: true,
+            postUrl: true,
+            countLikes: true,
+            countDislikes: true,
+        },
+    });
 
+    const allTagNames = await prisma.dimTags.findMany({
+        select: {
+            postId: true,
+            tagName: true,
+        },
+        where: { postId: { in: recentVotedPosts.map((post) => post.postId) } },
+    });
+
+    postData = recentVotedPosts.map((post) => {
+        const tagNames = allTagNames
+        .filter((tag) => tag.postId === post.postId)
+        .map((tag) => tag.tagName);
+        return { ...post, tagNames };
+    });
+  }
+  else {
+    const mostRecentPosts = await prisma.userPostContent.findMany({
+        orderBy: { postDateJst: "desc" },
+        skip: (pagingNumber - 1) * 10,
+        take: 10,
+        select: {
+          postId: true,
+          postTitle: true,
+          postDateJst: true,
+          postUrl: true,
+          countLikes: true,
+          countDislikes: true,
+        },
+      });
+
+      const allTagNames = await prisma.dimTags.findMany({
+        select: {
+          postId: true,
+          tagName: true,
+        },
+        where: { postId: { in: mostRecentPosts.map((post) => post.postId) } },
+      });
+    
+      postData = mostRecentPosts.map((post) => {
+        const tagNames = allTagNames
+          .filter((tag) => tag.postId === post.postId)
+          .map((tag) => tag.tagName);
+        return { ...post, tagNames };
+      });
+  }
+  
   return json({ mostRecentPosts: postData, currentPage: pagingNumber });
 }
 
 export async function action({ request }: ActionFunctionArgs) {
-  const formData = await request.formData();
-  const action = formData.get("action");
-  const currentPage = parseInt(formData.get("currentPage") as string);
-
-  if (action === "prevPage") {
-    return redirect(`/feed?p=${currentPage - 1}`);
-  } else if (action === "nextPage") {
-    return redirect(`/feed?p=${currentPage + 1}`);
-  }
-  return null;
+    const formData = await request.formData();
+    const action = formData.get("action");
+    const sort = formData.get("sort");
+    const currentPage = parseInt(formData.get("currentPage") as string);
+    const url = new URL(request.url);
+    const likeFrom = url.searchParams.get("likeFrom");
+    const likeTo = url.searchParams.get("likeTo");
+  
+    if (sort === "likes") {
+      return redirect(`/feed?p=1&likeFrom=24&likeTo=0`);
+    } else if (sort === "new") {
+      return redirect(`/feed?p=1`);
+    }
+  
+    if (!likeFrom && action === "prevPage") {
+      return redirect(`/feed?p=${currentPage - 1}`);
+    } else if (!likeFrom && action === "nextPage") {
+      return redirect(`/feed?p=${currentPage + 1}`);
+    } else if (likeFrom && action === "prevPage") {
+      return redirect(`/feed?likeFrom=${likeFrom}&likeTo=${likeTo}&p=${currentPage - 1}`);
+    } else if (likeFrom && action === "nextPage") {
+      return redirect(`/feed?likeFrom=${likeFrom}&likeTo=${likeTo}&p=${currentPage + 1}`);
+    }
+    return null;
 }
 
 export default function Feed() {
   const { mostRecentPosts, currentPage } = useLoaderData<typeof loader>();
-  const [searchParams] = useSearchParams();
 
   return (
     <div>
       <h1>Feed</h1>
+      <Form method="post" className="mb-4">
+        <div className="flex items-center">
+          <label htmlFor="sort-likes" className="mr-2">
+            <input
+              type="radio"
+              id="sort-likes"
+              name="sort"
+              value="likes"
+              className="mr-1"
+            />
+            いいね順
+          </label>
+          <label htmlFor="sort-new">
+            <input
+              type="radio"
+              id="sort-new"
+              name="sort"
+              value="new"
+              defaultChecked
+              className="mr-1"
+            />
+            新着順
+          </label>
+          <button
+            type="submit"
+            className="ml-4 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+          >
+            適用
+          </button>
+        </div>
+      </Form>
       {mostRecentPosts.map((post) => (
         <PostCard
           key={post.postId}
