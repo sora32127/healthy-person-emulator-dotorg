c83c7af09db65d660fa92b33ce9d43816e3ea707
contradiction29 <sora32127@gmail.com>
2024-03-28 17:25:05 +0900

    Implement Top10Voted in Toppage

--- app/routes/_layout._index.tsx
+++ app/routes/_layout._index.tsx
@@ -25,29 +25,69 @@ export async function loader() {
         },
     });
 
-    
+    const recentVotedPostsAgg = await prisma.fctPostVoteHisotry.groupBy({
+        by: ["postId"],
+        where: { 
+            voteDateGmt : { 
+                gte: new Date("2024-03-12"),
+                lte: new Date("2024-03-13")
+            },
+            voteTypeInt : { in : [1]}
+        },
+        _count: { voteUserIpHash: true },
+        orderBy: { _count: { voteUserIpHash: "desc" } },
+        take: 10,
+    });
 
-    const allTagNames = await prisma.dimTags.findMany({
+    const recentVotedPosts = await prisma.userPostContent.findMany({
+        where: { postId: { in: recentVotedPostsAgg.map((post) => post.postId) } },
+        select: {
+            postId: true,
+            postTitle: true,
+            postDateJst: true,
+            postUrl: true,
+            countLikes: true,
+            countDislikes: true,
+        },
+    });
+
+    let allPostIds = mostRecentPosts.map((post) => post.postId);
+    allPostIds = allPostIds.concat(recentVotedPosts.map((post) => post.postId));
+
+    const allTagsNames = await prisma.dimTags.findMany({
         select: {
             postId: true,
             tagName: true,
         },
-        where : { postId: { in: mostRecentPosts.map((post) => post.postId) } },
+        where: { postId: { in: allPostIds } },
     });
 
-    const postData = mostRecentPosts.map((post) => {
-        const tagNames = allTagNames
+    const mostRecentPostsWithTags = mostRecentPosts.map((post) => {
+        const tagNames = allTagsNames
             .filter((tag) => tag.postId === post.postId)
             .map((tag) => tag.tagName);
         return { ...post, tagNames };
-    });
+    }
+    );
 
-    return json({ mostRecentPosts: postData });
+    const recentVotedPostsWithTags = recentVotedPosts.map((post) => {
+        const tagNames = allTagsNames
+            .filter((tag) => tag.postId === post.postId)
+            .map((tag) => tag.tagName);
+        return { ...post, tagNames };
+    }
+    );
+
+
+    return json({
+        mostRecentPosts: mostRecentPostsWithTags,
+        recentVotedPosts: recentVotedPostsWithTags
+    });
 }
 
 
 export default function Feed() {
-    const { mostRecentPosts } = useLoaderData<typeof loader>();
+    const { mostRecentPosts, recentVotedPosts } = useLoaderData<typeof loader>();
     return (
         <>
         <H1>フィード</H1>
@@ -65,6 +105,19 @@ export default function Feed() {
             />
         ))}
         <NavLink to="/feed?p=2">次へ</NavLink>
+        <H2>最近いいねされた投稿</H2>
+        {recentVotedPosts.map((post) => (
+            <PostCard
+                key={post.postUrl}
+                postId={post.postId}
+                postTitle={post.postTitle}
+                postDateJst={post.postDateJst}
+                postUrl={post.postUrl}
+                tagNames={post.tagNames}
+                countLikes={post.countLikes}
+                countDislikes={post.countDislikes}
+            />
+        ))}
         </>
 
     );
