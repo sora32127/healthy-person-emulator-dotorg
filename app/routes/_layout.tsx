5cb643946fc693010b7a2514d98184864e7349af
contradiction29 <sora32127@gmail.com>
2024-03-29 11:27:06 +0900

    Fix indent

--- app/routes/_layout.tsx
+++ app/routes/_layout.tsx
@@ -14,75 +14,75 @@ export default function Component() {
       <div className="md:mt-32 md:mx-auto">
         <Outlet />
       </div>
-        <nav className="mx-auto px-4 py-4 bg-white shadow-inner mt-auto md:fixed md:top-0 md:w-full w-full">
-          <ul className="flex justify-around">
-            <li>
-              <NavLink
-                to="/"
-                className={({ isActive }) =>
-                  `block text-center text-gray-700 hover:text-blue-500 ${
-                    isActive ? "text-blue-500 font-bold" : ""
-                  }`
-                }
-              >
-                <img src="/src/assets/feed_icon.svg" alt="Feed" className="mx-auto" />
-                フィード
-              </NavLink>
-            </li>
-            <li>
-              <NavLink
-                to="/readme"
-                className={({ isActive }) =>
-                  `block text-center text-gray-700 hover:text-blue-500 ${
-                    isActive ? "text-blue-500 font-bold" : ""
-                  }`
-                }
-              >
-                <img src="/src/assets/guideline_icon.svg" alt="Readme" className="mx-auto" />
-                ガイドライン
-              </NavLink>
-            </li>
-            <li>
-              <NavLink
-                to="/post"
-                className={({ isActive }) =>
-                  `block text-center text-gray-700 hover:text-blue-500 ${
-                    isActive ? "text-blue-500 font-bold" : ""
-                  }`
-                }
-              >
-                <img src="/src/assets/post_icon.svg" alt="Post" className="mx-auto" />
-                投稿
-              </NavLink>
-            </li>
-            <li>
-              <NavLink
-                to="/search"
-                className={({ isActive }) =>
-                  `block text-center text-gray-700 hover:text-blue-500 ${
-                    isActive ? "text-blue-500 font-bold" : ""
-                  }`
-                }
-              >
-                <img src="/src/assets/search_icon.svg" alt="Search" className="mx-auto" />
-                検索
-              </NavLink>
-            </li>
-            <li>
-              <NavLink
-                to="/etc"
-                className={({ isActive }) =>
-                  `block text-center text-gray-700 hover:text-blue-500 ${
-                    isActive ? "text-blue-500 font-bold" : ""
-                  }`
-                }
-              >
-                <img src="/src/assets/menu_icon.svg" alt="Etc" className="mx-auto" />
-                メニュー
-              </NavLink>
-            </li>
-          </ul>
-        </nav>
+      <nav className="mx-auto px-4 py-4 bg-white shadow-inner mt-auto md:fixed md:top-0 md:w-full w-full">
+        <ul className="flex justify-around">
+          <li>
+            <NavLink
+              to="/"
+              className={({ isActive }) =>
+                `block text-center text-gray-700 hover:text-blue-500 ${
+                  isActive ? "text-blue-500 font-bold" : ""
+                }`
+              }
+            >
+              <img src="/src/assets/feed_icon.svg" alt="Feed" className="mx-auto" />
+              フィード
+            </NavLink>
+          </li>
+          <li>
+            <NavLink
+              to="/readme"
+              className={({ isActive }) =>
+                `block text-center text-gray-700 hover:text-blue-500 ${
+                  isActive ? "text-blue-500 font-bold" : ""
+                }`
+              }
+            >
+              <img src="/src/assets/guideline_icon.svg" alt="Readme" className="mx-auto" />
+              ガイドライン
+            </NavLink>
+          </li>
+          <li>
+            <NavLink
+              to="/post"
+              className={({ isActive }) =>
+                `block text-center text-gray-700 hover:text-blue-500 ${
+                  isActive ? "text-blue-500 font-bold" : ""
+                }`
+              }
+            >
+              <img src="/src/assets/post_icon.svg" alt="Post" className="mx-auto" />
+              投稿
+            </NavLink>
+          </li>
+          <li>
+            <NavLink
+              to="/search"
+              className={({ isActive }) =>
+                `block text-center text-gray-700 hover:text-blue-500 ${
+                  isActive ? "text-blue-500 font-bold" : ""
+                }`
+              }
+            >
+              <img src="/src/assets/search_icon.svg" alt="Search" className="mx-auto" />
+              検索
+            </NavLink>
+          </li>
+          <li>
+            <NavLink
+              to="/etc"
+              className={({ isActive }) =>
+                `block text-center text-gray-700 hover:text-blue-500 ${
+                  isActive ? "text-blue-500 font-bold" : ""
+                }`
+              }
+            >
+              <img src="/src/assets/menu_icon.svg" alt="Etc" className="mx-auto" />
+              メニュー
+            </NavLink>
+          </li>
+        </ul>
+      </nav>
     </div>
   );
 }
