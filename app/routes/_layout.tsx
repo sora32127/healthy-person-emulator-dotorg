d899b8b43882200972ff65b160d3fde608d8117d
contradiction29 <sora32127@gmail.com>
2024-03-29 12:47:10 +0900

    Add footer

--- app/routes/_layout.tsx
+++ app/routes/_layout.tsx
@@ -11,9 +11,10 @@ export default function Component() {
           <div className="w-16 h-16 border-4 border-blue-500 border-solid rounded-full animate-spin"></div>
         </div>
       )}
-      <div className="mx-4 md:mt-32 md:mx-10 lg:mx-20 xl:mx-40 2xl:mx-60">
+      <div className="mx-4 mb-32 md:mt-32 md:mx-10 lg:mx-20 xl:mx-40 2xl:mx-60">
         <Outlet />
       </div>
+      
       <nav className="fixed py-4 bottom-0 bg-white shadow-inner md:fixed md:top-0 md:w-full w-full md:bottom-auto">
         <ul className="flex justify-around">
           <li>
@@ -83,6 +84,22 @@ export default function Component() {
           </li>
         </ul>
       </nav>
+      <footer className="bg-gray-100 py-8 mt-auto">
+        <div className="container mx-auto px-4">
+          <div className="flex flex-col items-center">
+            <h2 className="text-2xl font-bold mb-4">健常者エミュレータ事例集</h2>
+            <nav className="mb-4">
+              <NavLink
+                to="/dummy-contact-url"
+                className="text-gray-700 hover:text-blue-500"
+              >
+                管理人に連絡する
+              </NavLink>
+            </nav>
+            <p className="text-gray-600 text-center">&copy; {new Date().getFullYear()} All rights reserved.</p>
+          </div>
+        </div>
+      </footer>
     </div>
   );
 }
