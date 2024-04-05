176b4c698f826ce48ff22172c431a7bf3800eb25
contradiction29 <sora32127@gmail.com>
2024-04-06 00:10:18 +0900

    Change anon from service role for security reason

--- app/modules/supabase.server.ts
+++ app/modules/supabase.server.ts
@@ -1,12 +1,12 @@
 import { createClient } from '@supabase/supabase-js';
 
 const SUPABASE_URL = process.env.SUPABASE_URL;
-const SUPABASE_ANON_KEY = process.env.SUPABASE_ANON_KEY;
+const SUPABASE_SERVICE_ROLE_KEY = process.env.SUPABASE_SERVICE_ROLE_KEY;
 
-if (!SUPABASE_URL || !SUPABASE_ANON_KEY) {
-    throw new Error('Supabase URL or Supabase Anon Key is missing.');
+if (!SUPABASE_URL || !SUPABASE_SERVICE_ROLE_KEY) {
+    throw new Error('Supabase URL or Supabase Service Role Key is missing.');
 }
 
-const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
+const supabase = createClient(SUPABASE_URL, SUPABASE_SERVICE_ROLE_KEY);
 
 export { supabase }
