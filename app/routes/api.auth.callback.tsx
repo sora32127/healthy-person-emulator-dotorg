fecbb9574a004e763462edceacf85db4f58926cd
contradiction29 <sora32127@gmail.com>
2024-04-05 23:20:20 +0900

    Simplify login flow

--- app/routes/api.auth.callback.tsx
+++ app/routes/api.auth.callback.tsx
@@ -1,14 +1,27 @@
-import { ActionFunctionArgs, redirect } from "@remix-run/node";
-import { useFetcher, useSearchParams } from "@remix-run/react";
+import { ActionFunctionArgs, json, redirect } from "@remix-run/node";
+import { useFetcher, useLoaderData, useSearchParams } from "@remix-run/react";
 import { useEffect } from "react";
 import { commitSession, getSession } from "~/modules/session.server";
-import { supabase } from "~/modules/supabase.client";
+import { createClient } from '@supabase/supabase-js';
+
 
 import type {
     Session,
 } from '@supabase/supabase-js';
 
+export async function loader () {
+    return json({ ENV : {
+        SUPABASE_URL_CLIENT : process.env.SUPABASE_URL_CLIENT,
+        SUPABASE_ANON_KEY_CLIENT : process.env.SUPABASE_ANON_KEY_CLIENT,
+    } });
+}
+
 export default function AuthCallBack() {
+    const { ENV } = useLoaderData<typeof loader>();
+    if (!ENV.SUPABASE_URL_CLIENT || !ENV.SUPABASE_ANON_KEY_CLIENT) {
+        throw new Error('Supabase URL or Supabase Anon Key is missing.');
+    }
+    const supabase = createClient(ENV.SUPABASE_URL_CLIENT, ENV.SUPABASE_ANON_KEY_CLIENT)
     const fetcher = useFetcher();
     const searchParams = useSearchParams();
     useEffect(() => {
@@ -39,17 +52,24 @@ export async function action({ request } : ActionFunctionArgs) {
     console.log("SupabaseSession", SupabaseSession)
 
     const session = await getSession(request.headers.get("Cookie"));
-    const {
-        access_token : accessToken,
-        refresh_token : refreshToken,
-        user,
-    } = SupabaseSession;
-    session.set("accessToken", accessToken);
-    session.set("refreshToken", refreshToken);
-    session.set("userId", user.id);
-    return redirect("/resetPassword", {
-        headers: {
-            "Set-Cookie": await commitSession(session),
-        },
-    });
+    try {
+        const {
+            access_token : accessToken,
+            refresh_token : refreshToken,
+            user,
+        } = SupabaseSession;
+        session.set("accessToken", accessToken);
+        session.set("refreshToken", refreshToken);
+        session.set("userId", user.id);
+        return redirect("/resetPassword", {
+            headers: {
+                "Set-Cookie": await commitSession(session),
+            },
+        });
+    } catch (error) {
+        console.error("error", error)
+        return redirect("/login");
+    }
+    
+    
 }
