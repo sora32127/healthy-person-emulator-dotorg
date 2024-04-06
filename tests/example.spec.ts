f017e8b1443cdcaa86f23e6a3c8c95127769babc
contradiction29 <sora32127@gmail.com>
2024-04-06 23:29:19 +0900

    Remove unused files

--- tests/example.spec.ts
+++
@@ -1,8 +0,0 @@
-import { test, expect } from '@playwright/test';
-
-const localHostUrl = 'http://localhost:5173';
-
-test('ユーザーは記事を閲覧できる', async ({ page }) => {
-  await page.goto(localHostUrl);
-  await expect(page).toHaveTitle(/健常者エミュレータ事例集/);
-});
