439cdd7e0fb3766f661227f9c33e64e6603280a8
contradiction29 <sora32127@gmail.com>
2024-04-06 22:48:52 +0900

    Tighten tag layout

--- app/components/TagCard.tsx
+++ app/components/TagCard.tsx
@@ -8,7 +8,7 @@ export default function TagCard({
     tagName,
 }: TagCardProps) {
     return (
-        <div className="rounded-md p-1 mb-4">
+        <div className="rounded-md p-1 mb-1">
             <NavLink to={`/tags/${tagName}`} className="text-lg font-bold text-blue-700 underline underline-offset-4 decoration-blue-700">{tagName}</NavLink>
         </div>
     );
