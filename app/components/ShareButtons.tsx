ff6a1268b8aed9efdbc6704961ae91487a6e740f
contradiction29 <sora32127@gmail.com>
2024-04-07 23:05:45 +0900

    Update SNS Share button

--- app/components/ShareButtons.tsx
+++ app/components/ShareButtons.tsx
@@ -1,22 +1,49 @@
+import { useEffect } from "react";
+
 export default function ShareButtons() {
+    useEffect(() => {
+        const twitterScript = document.createElement("script");
+        twitterScript.src = "https://platform.twitter.com/widgets.js";
+        twitterScript.async = true;
+        document.body.appendChild(twitterScript);
+
+        const hatenaScript = document.createElement("script");
+        hatenaScript.src = "https://b.st-hatena.com/js/bookmark_button.js";
+        hatenaScript.type = "text/javascript";
+        hatenaScript.dataset.scripttype = "module";
+        document.body.appendChild(hatenaScript);
+
+        return () => {
+            document.body.removeChild(twitterScript);
+            document.body.removeChild(hatenaScript);
+        };
+    }, []);
+
     return (
-        <div className="flex justify-center space-x-4">
+        <div className="flex justify-center items-center space-x-4">
             <a
-                href="https://twitter.com/share?ref_src=twsrc%5Etfw" className="twitter-share-button" 
-                data-show-count="false">Tweet</a><script async src="https://platform.twitter.com/widgets.js"></script>
+                href="https://twitter.com/share?ref_src=twsrc%5Etfw"
+                className="twitter-share-button"
+                data-size="large"
+                data-lang="ja"
+                data-show-count="false"
+            >
+                Tweet
+            </a>
             <a
-                href="https://b.hatena.ne.jp/entry/" 
-                className="hatena-bookmark-button" 
-                data-hatena-bookmark-layout="vertical-normal" 
-                data-hatena-bookmark-lang="ja" title="このエントリーをはてなブックマークに追加">
+                href="https://b.hatena.ne.jp/entry/"
+                className="hatena-bookmark-button"
+                data-hatena-bookmark-layout="vertical-normal"
+                data-hatena-bookmark-lang="ja"
+                title="このエントリーをはてなブックマークに追加"
+            >
                 <img
                     src="https://b.st-hatena.com/images/v4/public/entry-button/button-only@2x.png"
-                    alt="このエントリーをはてなブックマークに追加" 
+                    alt="このエントリーをはてなブックマークに追加"
                     width="20"
                     height="20"
-                    />
+                />
             </a>
-                <script type="text/javascript" async src="https://b.st-hatena.com/js/bookmark_button.js"></script>
         </div>
     );
 }
