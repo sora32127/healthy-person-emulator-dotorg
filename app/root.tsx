342c9086f7f5a9ce47d43658e45f2bbce77cbb6d
contradiction29 <sora32127@gmail.com>
2024-04-08 16:30:44 +0900

    Add sitename setting

--- app/root.tsx
+++ app/root.tsx
@@ -30,6 +30,7 @@ export function Layout({ children }: { children: React.ReactNode }) {
       <head>
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta property="og:site_name" content="健常者エミュレータ事例集"></meta>
         <Meta />
         <Links />
       </head>
