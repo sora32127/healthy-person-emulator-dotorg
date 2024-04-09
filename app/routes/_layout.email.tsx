c049d8e07ccd14285eaad95a5f4551c8cbc10378
contradiction29 <sora32127@gmail.com>
2024-04-09 11:49:23 +0900

    Change email page

--- app/routes/_layout.email.tsx
+++ app/routes/_layout.email.tsx
@@ -1,7 +1,10 @@
+import { H1 } from "~/components/Headings"
+
 export default function Mail() {
     return (
       <div>
-        <p>確認メールを送信しました。</p>
+        <H1>ユーザー仮登録完了</H1>
+        <p>ユーザーの仮登録を完了しました。メールボックスを確認し、本登録を完了させてください。</p>
       </div>
     );
   }
