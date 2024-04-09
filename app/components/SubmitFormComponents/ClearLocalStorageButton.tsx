eabde987946c8b0b9e611a4979c9756fa74c9619
contradiction29 <sora32127@gmail.com>
2024-04-09 13:59:52 +0900

    Fix style for ClearInput button

--- app/components/SubmitFormComponents/ClearLocalStorageButton.tsx
+++ app/components/SubmitFormComponents/ClearLocalStorageButton.tsx
@@ -47,7 +47,7 @@ const ClearLocalStorageButton = ({ clearInputs }: ClearLocalStorageButtonProps) 
               <div className="bg-gray-50 px-4 py-3 sm:px-6 sm:flex sm:flex-row-reverse">
                 <button
                   type="button"
-                  className="inline-flex items-center px-4 py-2 border border-transparent text-base font-medium rounded-md shadow-sm text-white bg-blue-600 hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500"
+                  className="mt-3 w-full inline-flex justify-center rounded-md border border-gray-300 shadow-sm px-4 py-2 bg-blue-500 text-base font-medium text-white hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 sm:mt-0 sm:ml-3 sm:w-auto sm:text-sm"
                   onClick={handleClearLocalStorage}
                 >
                   はい
