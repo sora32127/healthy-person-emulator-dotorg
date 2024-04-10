193d4a21052af0610be6ed85643c970445d9a63d
contradiction29 <sora32127@gmail.com>
2024-04-10 10:08:56 +0900

    Change support text

--- app/routes/_layout.support.tsx
+++ app/routes/_layout.support.tsx
@@ -6,18 +6,17 @@ export default function BeSponsor() {
     return (
         <div className='text-center'>
           <H1>サポートする</H1>
-          <p>
-            健常者エミュレータ事例集は、個人の属性に寄らず誰もが暗黙知にアクセスできる社会を実現するプロジェクトです。
-            私たちは、一人でも多くの人々に知識を届け、より良い社会の実現に貢献したいと考えています。
-          </p>
-          <p>
-            しかし、このプロジェクトを維持・発展させるためには、皆様のサポートが不可欠です。
-            もしこのプロジェクトに共感していただけるなら、ぜひ支援をご検討ください。
-          </p>
-          <p>
-            以下のボタンから、500円で私たちのサポーターになることができます。
-            あなたの支援が、より多くの人々に知識を届ける原動力となります。
-          </p>
+          <p>健常者と非健常者の境目は存在しません。時と場合次第で、誰もが健常者となり、誰もが非健常者となります。</p>
+          <p>そんな世界の中で、誰もが暗黙知にアクセスでき、投稿でき、評価できる「プラットフォーム」を作るのが、健常者エミュレータ事例集が存在する目的です。健常者エミュレータ事例集は、個人の属性に寄らず、暗黙知を必要とするすべての人のために存在しています。</p>
+          <p>暗黙知を共有できる場所を作るためには、サーバー代をはじめとしたある程度の金銭が必要です。現在、健常者エミュレータ事例集の運営には以下のコストがかかっています。</p>
+          <br></br>
+          <ul className="list-disc list-inside text-center">
+            <li><a href="https://supabase.com/pricing" className='text-blue-600 underline underline-offset-4'>Supabase Pro Plan</a>: $25/月</li>
+            <li><a href="https://vercel.com/pricing" className='text-blue-600 underline underline-offset-4'>Vercel Pro Plan</a> : $20/月</li>
+            <li>ドメイン代(Cloudflare) : $10.11/年</li>
+          </ul>
+          <br></br>
+          <p>もし健常者エミュレータ事例集の理念に共感していただけるのなら、以下のボタンから寄付していただけると助かります。</p>
           <button className="mt-8 w-full">
             <NavLink to ="https://buy.stripe.com/8wM3cG2Diemj5Qk5kk"
               role="link"
@@ -33,7 +32,7 @@ export default function BeSponsor() {
 }
 
 export const meta: MetaFunction = () => {
-  const title = "スポンサーになる";
+  const title = "サポートする";
   const description = "健常者エミュレータ事例集をサポートしよう";
   const ogLocale = "ja_JP";
   const ogSiteName = "健常者エミュレータ事例集";
