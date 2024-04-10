66a1385e75e6a15475ed75e5bfab1003b4637f29
contradiction29 <sora32127@gmail.com>
2024-04-10 21:56:43 +0900

    Change post clear button background color in modal window #6
    「入力をクリア」ボタンを押したときに表示されるモーダルウィンドウ内の「はい」ボタンのホバー時の背景色がグレーになっており、同化して見づらくなっていたのを修正

--- app/components/SubmitFormComponents/ClearLocalStorageButton.tsx
+++ app/components/SubmitFormComponents/ClearLocalStorageButton.tsx
@@ -47,7 +47,7 @@ const ClearLocalStorageButton = ({ clearInputs }: ClearLocalStorageButtonProps) 
               <div className="bg-gray-50 px-4 py-3 sm:px-6 sm:flex sm:flex-row-reverse">
                 <button
                   type="button"
-                  className="mt-3 w-full inline-flex justify-center rounded-md border border-gray-300 shadow-sm px-4 py-2 bg-blue-500 text-base font-medium text-white hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 sm:mt-0 sm:ml-3 sm:w-auto sm:text-sm"
+                  className="mt-3 w-full inline-flex justify-center rounded-md border border-gray-300 shadow-sm px-4 py-2 bg-blue-500 text-base font-medium text-white hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 sm:mt-0 sm:ml-3 sm:w-auto sm:text-sm"
                   onClick={handleClearLocalStorage}
                 >
                   はい
