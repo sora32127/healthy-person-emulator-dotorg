5d145424c4316b64942910e70a9509ef78a3d570
contradiction29 <sora32127@gmail.com>
2024-04-19 17:03:23 +0900

    Add IconComponent for SVG Icon
    TailwindCSSでは、<img src={pathForSVG}>の形でSVG画像を取り込むとスタイルを適用できず、ダークモードにしたときにSVGのfillがBlackで固定されてしまう。
    Tailwindでスタイルを適用するためにはこうするしかなかった

---
+++ app/components/icons/TopIcon.tsx
@@ -0,0 +1,7 @@
+export default function TopIcon() {
+    return (
+        <svg xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 -960 960 960" width="24">
+            <path d="M480-80 160-400l56-56 224 224V-640h80v408l224-224 56 56-320 320Z"/>
+        </svg>
+    )
+}
