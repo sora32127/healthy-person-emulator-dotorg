bcaad4224209a7f252fdc86f890b3467060e7d30
contradiction29 <sora32127@gmail.com>
2024-04-24 17:28:56 +0900

    Integrate AIInputBox into StaticTextInputBox

--- app/components/SubmitFormComponents/StaticTextInput.tsx
+++ app/components/SubmitFormComponents/StaticTextInput.tsx
@@ -1,4 +1,5 @@
 import { Form } from "@remix-run/react";
+import TextInputBoxAI from "./TextInputBoxAI";
 
 interface StaticTextInputProps {
   row: number;
@@ -29,12 +30,11 @@ function StaticTextInput({
       const placeholder = placeholders[i] || "";
       inputs.push(
         <div key={i} className="mb-4">
-          <textarea
+          <TextInputBoxAI
+            className={`w-full py-2 placeholder-slate-500 rounded-lg focus:outline-none ${title}-${i}`}
+            parentComponentStateValue={parentComponentStateValues[i] || ""}
+            onInputChange={(value) => handleInputChange(i, value)}
             placeholder={placeholder}
-            value={parentComponentStateValues[i] || ""}
-            onChange={(e) => handleInputChange(i, e.target.value)}
-            className={`w-full px-3 py-2 placeholder-slate-500 border rounded-lg focus:outline-none ${title}-${i}`}
-            rows={4}
           />
         </div>
       );
