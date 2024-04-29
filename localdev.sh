c57523893591073a9fad26fcdf2f4411f0658e45
minamorl <minamorl@users.noreply.github.com>
2024-04-29 17:38:54 +0900

    Optimize performance by persisting data volume

--- localdev.sh
+++ localdev.sh
@@ -3,7 +3,7 @@
 until docker info;
     do sleep 1;
 done;
-touch .env
+npx supabase stop
 npx supabase start
 npx supabase status | awk '/API URL/ {print "SUPABASE_URL="$NF} /anon key/ {print "SUPABASE_ANON_KEY="$NF} /service_role key/ {print "SUPABASE_SERVICE_ROLE_KEY="$NF} /DB URL/ {print "SUPABASE_CONNECTION_STRING="$NF}' > .env
 npm run reset:db
