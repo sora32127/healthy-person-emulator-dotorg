7ae8d79bc2c2e78a44073d5027999ad99e6e5143
contradiction29 <sora32127@gmail.com>
2024-04-30 13:20:39 +0900

    Add error handling

--- app/routes/_layout.archives.edit.$postId.tsx
+++ app/routes/_layout.archives.edit.$postId.tsx
@@ -158,6 +158,10 @@ export default function EditPost() {
     );
   }
 
+  if (!postData) {
+    return <div>投稿が見つかりません</div>;
+  }
+
   const { postTitle } = postData;
 
   const [markdownContent, setMarkdownContent] = useState(postMarkdown);
