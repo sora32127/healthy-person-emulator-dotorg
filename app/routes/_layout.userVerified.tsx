ed6254d6a7c95a456d653b8c6c5f398e99afc75e
contradiction29 <sora32127@gmail.com>
2024-04-30 14:43:59 +0900

    Add userverified page

---
+++ app/routes/_layout.userVerified.tsx
@@ -0,0 +1,14 @@
+import { NavLink } from "@remix-run/react";
+import { H1 } from "~/components/Headings";
+
+export default function UserVerified() {
+    return (
+      <div>
+        <H1>ユーザー本登録完了</H1>
+        <div className="text-center">
+        <p>ユーザーの本登録が完了しました。</p>
+        <NavLink to="/login" className="text-info underline underline-offset-4">ログインページへ移動</NavLink>
+        </div>
+      </div>
+    );
+  }
