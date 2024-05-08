2f5a1a8c124f5b39296a575aacf21675e968f298
contradiction29 <sora32127@gmail.com>
2024-05-08 21:24:17 +0900

    Add: Copy link to clipboard button
    SNSでのシェアをやりやすくするため、クリップボードにコピーするボタンを追加した

--- app/components/ShareButtons.tsx
+++ app/components/ShareButtons.tsx
@@ -1,4 +1,6 @@
+import { useState } from "react";
 import XLogo from "~/src/assets/X_logo_2023_(white).png";
+import CopyToClipBoard from "./icons/CopyToClipboard";
 
 interface ShareButtonsProps {
     currentURL: string;
@@ -6,9 +8,17 @@ interface ShareButtonsProps {
 }
 
 export default function ShareButtons({ currentURL, postTitle }: ShareButtonsProps) {
+    const [copyStatus, setCopyStatus] = useState<"idle" | "copied">("idle");
     const twitterShareText = encodeURIComponent(`${postTitle}-健常者エミュレータ事例集`);
     const url = new URL(currentURL);
     const hatenaBlogUrl = url.hostname + url.pathname;
+    const copy = async (currentURL: string) => {
+        await navigator.clipboard.writeText(currentURL);
+        setCopyStatus("copied");
+        setTimeout(() => {
+            setCopyStatus("idle");
+        }, 2000);
+    }
 
     return (
         <div className="flex justify-center items-center space-x-4">
@@ -33,6 +43,18 @@ export default function ShareButtons({ currentURL, postTitle }: ShareButtonsProp
                     />
                 </a>
             </button>
+            <div className="relative inline-block group">
+                <button 
+                    type="button" 
+                    onClick={() => copy(currentURL)}
+                    className="hover:bg-base-300 rounded-full p-2 transition-colors duration-300"
+                >
+                    <CopyToClipBoard />
+                </button>
+                <span className="absolute bottom-full left-1/2 transform -translate-x-1/2 bg-base-200 text-xs py-1 px-2 rounded opacity-0 transition-opacity duration-300 group-hover:opacity-100 pointer-events-none whitespace-nowrap">
+                    {copyStatus === "idle" ? "クリップボードにコピー" : "クリップボードにコピーしました"}
+                </span>
+            </div>
         </div>
     );
 }
