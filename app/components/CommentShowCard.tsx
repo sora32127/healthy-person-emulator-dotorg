ccf933ec84a7aff174f0ae85f6998fc22adc519d
contradiction29 <sora32127@gmail.com>
2024-05-26 17:05:06 +0900

    Style: Add clock Icon for CommentShowCard

--- app/components/CommentShowCard.tsx
+++ app/components/CommentShowCard.tsx
@@ -2,6 +2,7 @@ import { NavLink } from "@remix-run/react";
 import CommentIcon from "./icons/CommentIcon";
 import ArticleIcon from "./icons/ArticleIcon";
 import RelativeDate from "./RelativeDate";
+import ClockIcon from "./icons/ClockIcon";
 
 interface CommentShowCardProps {
   commentContent: string;
@@ -23,22 +24,27 @@ export default function CommentShowCard({
 
   return (
       <div className="bg-base-100 border-2 rounded-lg p-4 mb-4">
-          <div className="flex justify-between items-center mb-2">
-              <RelativeDate timestamp={commentDateGmt} />
-              <span className="text-lg font-bold text-base-content comment-author">{commentAuthor}</span>
-          </div>
-          <div className="grid grid-cols-[auto_1fr] gap-2 mb-2 items-center">
-              <div className="w-6 h-6">
-                  <CommentIcon />
-              </div>
-              <p className="text-base-content comment-content">{commentContent}</p>
-          </div>
-          <div className="grid grid-cols-[auto_1fr] gap-2 items-center">
-              <div className="w-6 h-6">
-                  <ArticleIcon />
-              </div>
-              <NavLink to={`/archives/${postId}`} className="text-xl font-bold text-info underline underline-offset-4 post-title">{dimPosts.postTitle}</NavLink>
-          </div>
+        <div className="flex justify-between items-center mb-2">
+            <div className="flex my-1">
+                <div className="pr-2">
+                    <ClockIcon/>
+                </div>
+                <RelativeDate timestamp={commentDateGmt} />
+            </div>
+            <span className="text-lg font-bold text-base-content comment-author">{commentAuthor}</span>
+        </div>
+        <div className="grid grid-cols-[auto_1fr] gap-2 mb-2 items-center">
+            <div className="w-6 h-6">
+                <CommentIcon />
+            </div>
+            <p className="text-base-content comment-content">{commentContent}</p>
+        </div>
+        <div className="grid grid-cols-[auto_1fr] gap-2 items-center">
+            <div className="w-6 h-6">
+                <ArticleIcon />
+            </div>
+            <NavLink to={`/archives/${postId}`} className="text-xl font-bold text-info underline underline-offset-4 post-title">{dimPosts.postTitle}</NavLink>
+        </div>
       </div>
   );
 }
