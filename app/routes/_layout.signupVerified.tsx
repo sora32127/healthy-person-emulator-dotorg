86ed296b844fdff5d2f1f13aeff9b7f4bacb84bd
contradiction29 <sora32127@gmail.com>
2024-06-23 10:36:12 +0900

    Fix: Add signup verify page
    サインアップした後404に飛んでしまうため、サインアップ後のフローを追加した

---
+++ app/routes/_layout.signupVerified.tsx
@@ -0,0 +1,13 @@
+import { H1 } from "~/components/Headings";
+
+export default function SignupVerified() {
+    return (
+        <div>
+            <H1>サインアップが完了しました</H1>
+            <ul>
+                <li>ログインが完了しました</li>
+                <li>編集を行うことが可能です</li>
+            </ul>
+        </div>
+    )
+}
