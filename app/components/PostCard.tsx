d1663f37cbf8fc8ea850ba9228f6ee573e1eb30b
contradiction29 <sora32127@gmail.com>
2024-08-11 13:26:58 +0900

    Apply grid layout for PostCard

--- app/components/PostCard.tsx
+++ app/components/PostCard.tsx
@@ -38,43 +38,46 @@ export default function PostCard({
     const hiddenTagsCount = tagNames.length - displayedTags.length;
 
     return (
-        <div className="bg-base-100 p-4">
-            <div className="flex flex-raw my-2">
-                <div className="flex flex-raw">
-                    <RelativeDate timestamp={postDateGmt} />
-                </div>
-                <div className="flex items-center ml-3 gap-x-2">
+        <div className="bg-base-100 p-4 grid grid-cols-[1fr_minmax(auto,_800px)_1fr]">
+            <div className="col-span-1" />
+            <div className="mx-2">
+                <div className="flex flex-row my-2 justify-between">
                     <div className="flex items-center">
-                        <ThumbsUpIcon/>
-                        <span className="text-sm text-base-content ml-1">{countLikes}</span>
+                        <RelativeDate timestamp={postDateGmt} />
                     </div>
-                    <div className="flex items-center">
-                        <ThumbsDownIcon/>
-                        <span className="text-sm text-base-content ml-1">{countDislikes}</span>
+                    <div className="flex items-center gap-x-2">
+                        <div className="flex items-center">
+                            <ThumbsUpIcon/>
+                            <span className="text-sm text-base-content ml-1">{countLikes}</span>
+                        </div>
+                        <div className="flex items-center">
+                            <ThumbsDownIcon/>
+                            <span className="text-sm text-base-content ml-1">{countDislikes}</span>
+                        </div>
+                    </div>
+                </div>
+                <NavLink to={`/archives/${postId}`} className="hover:underline hover:underline-offset-4">
+                    <img src={`https://healthy-person-emulator-public-assets.s3.ap-northeast-1.amazonaws.com/${postId}.jpg`} alt={postTitle} className="w-full object-cover"/>
+                    <div className="mt-1 mb-2">
+                        <p className="text-lg">{postTitle}</p>
+                    </div>
+                </NavLink>
+                <div>
+                    <div className="flex flex-wrap items-center">
+                        {displayedTags.map((tag, index) => (
+                            <span key={`${tag}-${postId}`} className="inline-block mr-1 mb-1">
+                                <TagCard tagName={tag} />
+                            </span>
+                        ))}
+                        {hiddenTagsCount > 0 && (
+                            <span className="inline-block text-base-content">
+                                (+{hiddenTagsCount}タグ)
+                            </span>
+                        )}
                     </div>
                 </div>
             </div>
-            <NavLink to={`/archives/${postId}`} className="hover:underline hover:underline-offset-4">
-                <img src={`https://healthy-person-emulator-public-assets.s3.ap-northeast-1.amazonaws.com/${postId}.jpg`} alt={postTitle}/>
-                <div className="mt-1 mb-2">
-                    <p className="text-lg">{postTitle}</p>
-                </div>
-            </NavLink>
-            <div>
-                <div className="flex flex-wrap items-center">
-                    {displayedTags.map((tag, index) => (
-                        <span key={`${tag}-${postId}`} className="inline-block mr-1 mb-1">
-                            <TagCard tagName={tag} />
-                        </span>
-                    ))}
-                    {hiddenTagsCount > 0 && (
-                        <span className="inline-block text-base-content">
-                            (+{hiddenTagsCount}タグ)
-                        </span>
-                    )}
-                </div>
-            </div>
-
+            <div className="col-span-1" />
         </div>
     );
 }
