f15ae69c7f797a778d7cb77d573426d53784a796
contradiction29 <sora32127@gmail.com>
2024-08-19 22:20:45 +0900

    Bugfix: Fix rendering bug

--- app/routes/_layout.archives.$postId.tsx
+++ app/routes/_layout.archives.$postId.tsx
@@ -190,13 +190,9 @@ export default function Component() {
       <div>
         <H1>{data.postTitle}</H1>
         <div>
-          <div className="grid grid-cols-[auto_1fr] gap-2 my-1 items-center">
-            <div className="w-6 h-6">
-              <ClockIcon />
-            </div>
-            <p>
-              <RelativeDate timestamp={data.postDateGmt} />
-            </p>
+          <div className="flex items-start gap-2 my-1">
+            <div className="w-6 h-6"><ClockIcon /></div>
+            <RelativeDate timestamp={data.postDateGmt} />
           </div>
           <div className="grid grid-cols-[auto_1fr] gap-2 mb-2 items-center">
             <div className="w-6 h-6">
