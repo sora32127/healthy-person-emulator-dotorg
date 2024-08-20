67f87c7ce53b4097450e89d70a7311e3a41ff97f
contradiction29 <sora32127@gmail.com>
2024-08-20 14:05:31 +0900

    Style: Add feedback for themechange

--- app/components/ThemeSwitcher.tsx
+++ app/components/ThemeSwitcher.tsx
@@ -1,8 +1,10 @@
 import { useEffect, useState } from "react";
 import { MdOutlineLightMode, MdOutlineDarkMode } from "react-icons/md";
+import { AiOutlineLoading3Quarters } from "react-icons/ai";
 
 export default function ThemeSwitcher() {
   const [nowTheme, setNowTheme] = useState<string>();
+  const [isChanging, setIsChanging] = useState(false);
 
   useEffect(() => {
     if (window.localStorage.getItem("theme") === null) {
@@ -15,16 +17,18 @@ export default function ThemeSwitcher() {
   }, []);
 
   const toggleTheme = () => {
+    setIsChanging(true);
     const newTheme = nowTheme === "dark" ? "light" : "dark";
     window.localStorage.setItem("theme", newTheme);
     setNowTheme(newTheme);
     document.querySelector("html")?.setAttribute("data-theme", newTheme);
+    setTimeout(() => setIsChanging(false), 500);
   };
 
   return (
     <div className="tooltip tooltip-bottom" data-tip={nowTheme === "dark" ? "ライトモードに切り替え" : "ダークモードに切り替え"}>
-      <button onClick={toggleTheme} type="button" className="btn btn-ghost">
-        {nowTheme === "dark" ? <MdOutlineLightMode className="w-6 h-6" /> : <MdOutlineDarkMode className="w-6 h-6" />}
+      <button onClick={toggleTheme} type="button" className={`btn btn-circle ml-1 mt-1 ${isChanging ? 'animate-spin' : ''}`}>
+        {isChanging ? <AiOutlineLoading3Quarters className="w-6 h-6 animate-spin" /> : nowTheme === "dark" ? <MdOutlineLightMode className="w-6 h-6" /> : <MdOutlineDarkMode className="w-6 h-6" />}
       </button>
     </div>
   );
