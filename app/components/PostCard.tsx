296831554d60e95f503d17af517fc7e8ea7970a0
contradiction29 <sora32127@gmail.com>
2024-08-21 12:17:13 +0900

    Performance: Specify img width, height, aspect ratio to avoid Content Layout Shift

--- app/components/PostCard.tsx
+++ app/components/PostCard.tsx
@@ -74,7 +74,14 @@ export default function PostCard({
                     </div>
                 </div>
                 <NavLink to={`/archives/${postId}`} className="hover:underline hover:underline-offset-4">
-                    <img src={`https://healthy-person-emulator-public-assets.s3.ap-northeast-1.amazonaws.com/${postId}.jpg`} alt={postTitle} className="w-full object-cover" loading="lazy"/>
+                    <img
+                        src={`https://healthy-person-emulator-public-assets.s3.ap-northeast-1.amazonaws.com/${postId}.jpg`}
+                        alt={postTitle}
+                        loading="lazy"
+                        width="620"
+                        height="300"
+                        className="aspect-[620/300]"
+                    />
                     <div className="mt-1 mb-2">
                         <p className="text-xl font-bold">{postTitle}</p>
                     </div>
