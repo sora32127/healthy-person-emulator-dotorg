2e6f524d5063bfc98050d826b11df71003631774
contradiction29 <sora32127@gmail.com>
2024-08-27 17:08:32 +0900

    Style: Add title for SVG element

--- app/components/SubmitFormComponents/TagSelectionBox.tsx
+++ app/components/SubmitFormComponents/TagSelectionBox.tsx
@@ -57,6 +57,7 @@ const TagSelectionBox = ({
                         className="input input-bordered w-full py-2 pl-10 pr-3 placeholder-slate-500"
                     />
                     <svg className="absolute left-3 top-1/2 transform -translate-y-1/2 w-5 h-5 text-gray-400" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                        <title>タグを検索</title>
                         <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" />
                     </svg>
                 </div>
@@ -122,6 +123,7 @@ const TagSelectionBox = ({
                                 viewBox="0 0 24 24"
                                 stroke="currentColor"
                             >
+                                <title>タグを削除</title>
                                 <path
                                     strokeLinecap="round"
                                     strokeLinejoin="round"
