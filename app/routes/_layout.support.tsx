b91e2861d2382e8bbff53d3314efb816ddebeb97
contradiction29 <sora32127@gmail.com>
2024-10-26 12:46:05 +0900

    Modulaize metafunction in non-dynamic routes

--- app/routes/_layout.support.tsx
+++ app/routes/_layout.support.tsx
@@ -1,6 +1,8 @@
-import { MetaFunction, NavLink } from '@remix-run/react';
+import type { MetaFunction } from '@remix-run/node';
+import { NavLink } from '@remix-run/react';
 import { H1 } from '~/components/Headings';
 import fanboxQRCode from '~/src/assets/fanbox_qr_code.png';
+import { commonMetaFunction } from '~/utils/commonMetafunction';
 
 export default function BeSponsor() {
     return (
@@ -9,13 +11,13 @@ export default function BeSponsor() {
           <p>健常者と非健常者の境目は存在しません。時と場合次第で、誰もが健常者となり、誰もが非健常者となります。</p>
           <p>そんな世界の中で、誰もが暗黙知にアクセスでき、投稿でき、評価できる「プラットフォーム」を作るのが、健常者エミュレータ事例集が存在する目的です。健常者エミュレータ事例集は、個人の属性に寄らず、暗黙知を必要とするすべての人のために存在しています。</p>
           <p>暗黙知を共有できる場所を作るためには、サーバー代をはじめとしたある程度の金銭が必要です。現在、健常者エミュレータ事例集の運営には以下のコストがかかっています。</p>
-          <br></br>
+          <br/>
           <ul className="list-disc list-inside text-center">
             <li><a href="https://supabase.com/pricing" className='text-info underline underline-offset-4'>Supabase Pro Plan</a>: $25/月</li>
             <li><a href="https://vercel.com/pricing" className='text-info underline underline-offset-4'>Vercel Pro Plan</a> : $20/月</li>
             <li>ドメイン代(Cloudflare) : $10.11/年</li>
           </ul>
-          <br></br>
+          <br/>
           <p>もし健常者エミュレータ事例集の理念に共感していただけるのなら、以下のPixiv Fanboxから寄付していただけると助かります。</p>
           <NavLink to="https://contradiction29.fanbox.cc/"
             role="link"
@@ -30,35 +32,11 @@ export default function BeSponsor() {
 }
 
 export const meta: MetaFunction = () => {
-  const title = "サポートする";
-  const description = "健常者エミュレータ事例集をサポートしよう";
-  const ogLocale = "ja_JP";
-  const ogSiteName = "健常者エミュレータ事例集";
-  const ogType = "article";
-  const ogTitle = title;
-  const ogDescription = description;
-  const ogUrl = `https://healthy-person-emulator.org/support`;
-  const twitterCard = "summary"
-  const twitterSite = "@helthypersonemu"
-  const twitterTitle = title
-  const twitterDescription = description
-  const twitterCreator = "@helthypersonemu"
-  const twitterImage = "https://qc5axegmnv2rtzzi.public.blob.vercel-storage.com/favicon-CvNSnEUuNa4esEDkKMIefPO7B1pnip.png"
-
-  return [
-    { title },
-    { description },
-    { property: "og:title", content: ogTitle },
-    { property: "og:description", content: ogDescription },
-    { property: "og:locale", content: ogLocale },
-    { property: "og:site_name", content: ogSiteName },
-    { property: "og:type", content: ogType },
-    { property: "og:url", content: ogUrl },
-    { name: "twitter:card", content: twitterCard },
-    { name: "twitter:site", content: twitterSite },
-    { name: "twitter:title", content: twitterTitle },
-    { name: "twitter:description", content: twitterDescription },
-    { name: "twitter:creator", content: twitterCreator },
-    { name: "twitter:image", content: twitterImage },
-  ];
+  const commonMeta = commonMetaFunction({
+    title : "サポートする",
+    description : "健常者エミュレータ事例集をサポートしよう",
+    url: "https://healthy-person-emulator.org/support",
+    image: null
+  });
+  return commonMeta;
 };
