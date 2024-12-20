69662f430d6f2e5f4307c149f833a7f5e48eb8d1
contradiction29 <sora32127@gmail.com>
2024-12-20 17:56:59 +0900

    Delete: Remove unused files

--- tests/test-1.spec.ts
+++
@@ -1,6 +0,0 @@
-import { test, expect } from '@playwright/test';
-
-test('test', async ({ page }) => {
-  await page.goto('http://localhost:3000/');
-  await page.locator('a > .btn').click();
-});
--- tests/test-2.spec.ts
+++
@@ -1,9 +0,0 @@
-import { test, expect } from '@playwright/test';
-
-test('test', async ({ page }) => {
-  await page.goto('http://localhost:3000/');
-  await page.locator('a > .btn').click();
-  await page.getByLabel('結果善：').check();
-  await page.getByLabel('結果悪：').check();
-  await page.getByLabel('結果善：').check();
-});
