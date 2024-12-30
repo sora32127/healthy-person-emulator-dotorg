949c63f2efef605ce6a00d954b5e43aa13dd719c
contradiction29 <sora32127@gmail.com>
2024-12-30 18:28:50 +0900

    Style: Change order of Menu

--- app/utils/itemMenu.ts
+++ app/utils/itemMenu.ts
@@ -3,12 +3,13 @@ import { House, Search, HandCoins, BookText, ThumbsUp, Notebook, Shuffle, LogOut
 export function getNavItems(isSignedIn: boolean){
     return [
         { to: "/?referrer=fromMenu", icon: House, text: "トップ" },
-        { to: "/search", icon: Search, text: "検索する" },
+        { to: "/?tab=fixed", text: "固定ページ", icon: Notebook },
+        { to: "/?tab=random", text: "ランダム", icon: Shuffle },
         { to: "/support", text: "サポートする", icon: HandCoins },
         { to: "/readme", text: "サイト説明", icon: BookText },
         { to: "/feed?p=1&type=unboundedLikes", text: "無期限いいね順", icon: ThumbsUp },
-        { to: "/?tab=fixed", text: "固定ページ", icon: Notebook },
-        { to: "/?tab=random", text: "ランダム", icon: Shuffle },
+        { to: "/search", icon: Search, text: "検索する" },
+
         ...(isSignedIn
           ? [{ to: "/logout", text: "ログアウト", icon: LogOut }]
           : [
