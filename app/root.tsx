abcd8cf6937ddeab012a8360ba1e80d91a735f72
contradiction29 <sora32127@gmail.com>
2025-01-02 10:27:21 +0900

    Fix: Remove google fonts and import font locally

--- app/root.tsx
+++ app/root.tsx
@@ -20,13 +20,7 @@ import { useCallback, useEffect } from "react";
 import { FaSpinner } from "react-icons/fa";
 
 export const links: LinksFunction = () => [
-  { rel: "stylesheet", href: stylesheet },
-  { rel: 'preconnect', href: 'https://fonts.googleapis.com' },
-  { rel: 'preconnect', href: 'https://fonts.gstatic.com', crossOrigin: 'anonymous' },
-  {
-    rel: 'stylesheet',
-    href: 'https://fonts.googleapis.com/css2?family=Noto+Sans+JP:wght@100..900&family=Ubuntu:ital,wght@0,300;0,400;0,500;0,700;1,300;1,400;1,500;1,700&display=swap',
-  },
+  { rel: "stylesheet", href: stylesheet }
 ];
 
 export const loader: LoaderFunction = (args) => rootAuthLoader(args);
