7585fb9a46c28e7e29f526a7e433eacbe352c5b7
contradiction29 <sora32127@gmail.com>
2025-01-03 11:47:44 +0900

    Feature: Update px into 1

--- app/components/CommonNavLink.tsx
+++ app/components/CommonNavLink.tsx
@@ -4,7 +4,7 @@ export function CommonNavLink({ to, children, className }: { to: string, childre
   return (
     <NavLink
       to={to}
-      className={`text-info underline underline-offset-4 hover:bg-base-200 rounded-md px-2 py-1 ${className}`}
+      className={`text-info underline underline-offset-4 hover:bg-base-200 rounded-md px-1 py-1 ${className}`}
       viewTransition
     >
       {children}
