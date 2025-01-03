52980051e173a2a859593a01f2760cb0aa466d47
contradiction29 <sora32127@gmail.com>
2025-01-03 14:07:00 +0900

    Package: Remove installGrobals

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,6 @@
 import { vitePlugin as remix } from "@remix-run/dev";
-import { installGlobals } from "@remix-run/node";
 import tsconfigPaths from "vite-tsconfig-paths";
 import { type ConfigEnv, defineConfig, loadEnv } from "vite";
-
-installGlobals();
 
 export default defineConfig(({ mode }: ConfigEnv) => {
   process.env = {...process.env, ...loadEnv(mode, process.cwd())};
