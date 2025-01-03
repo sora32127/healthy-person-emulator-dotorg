8c78ca3a11c1521db6dbfa15644bd1f31c783bc5
contradiction29 <sora32127@gmail.com>
2025-01-03 14:25:35 +0900

    Package: Enable v3_lazyRouteDiscorvery

--- vite.config.ts
+++ vite.config.ts
@@ -11,6 +11,7 @@ export default defineConfig(({ mode }: ConfigEnv) => {
           v3_fetcherPersist: true,
           v3_relativeSplatPath: true,
           v3_throwAbortReason: true,
+          v3_lazyRouteDiscovery: true,
         },
       }),
       tsconfigPaths(),
