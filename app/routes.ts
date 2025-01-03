5d817118907adc2ea1a32aa4fffce7609dc37f6f
contradiction29 <sora32127@gmail.com>
2025-01-03 19:48:58 +0900

    Feature: Update Router

--- app/routes.ts
+++ app/routes.ts
@@ -1,4 +1,4 @@
-import { type RouteConfig } from "@remix-run/route-config";
+import type { RouteConfig } from "@remix-run/route-config";
 import { remixRoutesOptionAdapter } from "@remix-run/routes-option-adapter";
 import { flatRoutes } from "remix-flat-routes";
 
