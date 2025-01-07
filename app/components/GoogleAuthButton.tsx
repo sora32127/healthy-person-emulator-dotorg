9d9d024122590368cf1c7c386b7cfe8166adbd07
contradiction29 <sora32127@gmail.com>
2025-01-07 19:56:34 +0900

    Rename: Google LoginButton to GoogleAuthButton

--- app/components/GoogleAuthButton.tsx
+++ app/components/GoogleAuthButton.tsx
@@ -1,14 +1,14 @@
+import { Link } from "lucide-react";
 import googleIcon from "~/components/icons/google_icon.svg";
 
-export default function GoogleLoginButton({ onClick }: { onClick: () => void }) {
+export default function GoogleLoginButton() {
   return (
-    <button
-        type="button"
-        onClick={onClick}
+    <Link
+        to="/api/googleLogin"
         className="flex items-center justify-start bg-base-200 rounded-md p-2 border border-gray-300 gap-x-10 hover:bg-base-300"
     >
       <img src={googleIcon} alt="GoogleIcon" className="w-10 h-10" />
       Googleで認証する
-    </button>
+    </Link>
   )
 }
