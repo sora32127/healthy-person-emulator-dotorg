591d39b325bf86414c5a73106f4c7b639df49cf1
contradiction29 <sora32127@gmail.com>
2025-01-07 19:49:35 +0900

    Feature: Remove email auth

--- app/routes/_layout.login.tsx
+++ app/routes/_layout.login.tsx
@@ -1,19 +1,9 @@
-import type { ActionFunctionArgs, LoaderFunctionArgs, MetaFunction } from "@remix-run/node";
+import type { LoaderFunctionArgs, MetaFunction } from "@remix-run/node";
 import { commonMetaFunction } from "~/utils/commonMetafunction";
-import { Form, useFetcher, useLoaderData } from "@remix-run/react";
+import { useFetcher, useLoaderData } from "@remix-run/react";
 import { H1 } from "~/components/Headings";
-import { useEffect } from "react";
-import { z } from "zod";
-import { useForm } from "react-hook-form";
-import { zodResolver } from "@hookform/resolvers/zod";
 import toast, { Toaster } from "react-hot-toast";
 import GoogleLoginButton from "~/components/GoogleLoginButton";
-
-export const loginSchema = z.object({
-  email: z.string().email(),
-  password: z.string().min(8),
-});
-type LoginSchema = z.infer<typeof loginSchema>;
 
 export async function loader({ request }: LoaderFunctionArgs) {
   const url = new URL(request.url)
@@ -25,57 +15,6 @@ export async function loader({ request }: LoaderFunctionArgs) {
 export default function login() {
   const { refferer } = useLoaderData<typeof loader>();
   const isRedirectedFromEditPost = refferer === "fromEditPost";
-  const isError = refferer === "error";
-  const loginFetcher = useFetcher();
-  const { register, handleSubmit, formState: { errors }, getValues } = useForm<LoginSchema>({
-    resolver: zodResolver(loginSchema),
-  });
-  const handleLogin = (e: React.MouseEvent<HTMLButtonElement>) => {
-    e.preventDefault();
-    const inputValues = getValues();
-    const formData = new FormData();
-    formData.append("type", "email-login");
-    formData.append("email", inputValues.email);
-    formData.append("password", inputValues.password);
-    loginFetcher.submit(formData, {
-      method: "post",
-      action: "/api/loginEmail",
-    });
-  }
-
-  const createUserFetcher = useFetcher();
-  const handleCreateUser = (e: React.MouseEvent<HTMLButtonElement>) => {
-    e.preventDefault();
-    const inputValues = getValues();
-    const formData = new FormData();
-    formData.append("type", "email-create-user");
-    formData.append("email", inputValues.email);
-    formData.append("password", inputValues.password);
-    createUserFetcher.submit(formData, {
-      method: "post",
-      action: "/api/signUpEmail",
-    });
-  }
-
-  useEffect(() => {
-    const response = loginFetcher.data as { success: boolean, message: string };
-    if (response?.success === false) {
-      toast.error(response.message);
-    }
-    if (response?.success === true) {
-      toast.success("ログインしました");
-    }
-  }, [loginFetcher.data]);
-
-  useEffect(() => {
-    const response = createUserFetcher.data as { success: boolean, message: string };
-    if (response?.success === false) {
-      toast.error(response.message);
-    }
-    if (response?.success === true) {
-      toast.success("新規登録しました");
-    }
-  }, [createUserFetcher.data]);
 
   const googleLoginFetcher = useFetcher();
   const handleGoogleLogin = () => {
@@ -93,53 +32,9 @@ export default function login() {
           <div className="card-body bg-error">
             <p className="text-error-content">編集するにはログインする必要があります</p>
           </div>}
-          {isError &&
-          <div className="card-body bg-error">
-            <p className="text-error-content">メールアドレスもしくはパスワードが一致しません</p>
-          </div>}
           <div className="card-body">
             <H1>ユーザー認証</H1>
             <GoogleLoginButton onClick={handleGoogleLogin}/>
-            <p className="text-center text-sm py-6">もしくは</p>
-            <Form method="post" action="/login" className="space-y-4">
-              <div className="form-control w-full">
-                <label className="label" htmlFor="email">
-                  <span className="label-text">メールアドレス</span>
-                </label>
-                <input 
-                  type="email" 
-                  id="email"
-                  required 
-                  className="input input-bordered w-full" 
-                  placeholder="something@example.com" 
-                  {...register("email")}
-                />
-              </div>
-
-              <div className="form-control w-full">
-                <label className="label" htmlFor="password">
-                  <span className="label-text">パスワード</span>
-                </label>
-                <input
-                  type="password"
-                  id="password"
-                  autoComplete="current-password"
-                  required
-                  className="input input-bordered w-full"
-                  placeholder="パスワードを入力"
-                  {...register("password")}
-                />
-              </div>
-              <div className="my-4 py-10">
-              <button type="button" className="btn btn-primary w-full" onClick={handleLogin}>
-                メールアドレスでログイン
-                </button>
-                {/* TODO: メールアドレスの新規登録は今後非推奨とする*/}
-                <button type="button" className="btn btn-primary w-full mt-6" onClick={handleCreateUser}>
-                  メールアドレスで新規登録
-                </button>
-              </div>
-            </Form>
           </div>
         </div>
       </div>
