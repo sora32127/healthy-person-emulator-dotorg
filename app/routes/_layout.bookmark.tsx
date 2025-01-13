aa70ed1cbf4fa97524131478b0c4663633ea4131
contradiction29 <sora32127@gmail.com>
2025-01-13 13:26:02 +0900

    Style: Update style of Bookmark route

--- app/routes/_layout.bookmark.tsx
+++ app/routes/_layout.bookmark.tsx
@@ -1,8 +1,10 @@
-import { MetaFunction, redirect, type LoaderFunctionArgs } from "@remix-run/node";
+import { type MetaFunction, redirect, type LoaderFunctionArgs } from "@remix-run/node";
 import { useLoaderData } from "@remix-run/react";
+import { CommonNavLink } from "~/components/CommonNavLink";
 import { authenticator } from "~/modules/auth.google.server";
-import { getBookmarkPosts, getUserId } from "~/modules/db.server";
+import { getBookmarkPosts, getUserId, PostCardData } from "~/modules/db.server";
 import { commonMetaFunction } from "~/utils/commonMetafunction";
+import { ThumbsUp, ThumbsDown, MessageSquareText } from 'lucide-react';
 
 export async function loader({ request }: LoaderFunctionArgs){
     const isAuthenticated = await authenticator.isAuthenticated(request);
@@ -22,12 +24,25 @@ export default function BookmarkLayout(){
             <p>ブックマークした記事</p>
             <p>メールアドレス: {email}</p>
             <p>ブックマーク数: {bookmarkPosts.length}</p>
-            <ul>
-                {bookmarkPosts.map((post) => (
-                    <li key={post.postId}>{post.postId}</li>
-                ))}
-            </ul>
+            <BookMarkView bookmarkPosts={bookmarkPosts} />
         </div>
+    )
+}
+
+function BookMarkView({ bookmarkPosts }: { bookmarkPosts: PostCardData[] }){
+    return (
+        <ul>
+            {bookmarkPosts.map((post) => (
+                <li key={post.postId} className="flex gap-x-3 my-2">
+                    <CommonNavLink to={`/archives/${post.postId}`}>{post.postTitle}</CommonNavLink>
+                    <div className="flex items-center gap-x-1">
+                        <ThumbsUp className="fill-none stroke-current gap-x-1" /> {post.countLikes}
+                        <ThumbsDown className="fill-none stroke-current gap-x-1" /> {post.countDislikes}
+                        <MessageSquareText className="fill-none stroke-current gap-x-1" /> {post.countComments}
+                    </div>
+                </li>
+            ))}
+        </ul>
     )
 }
 
