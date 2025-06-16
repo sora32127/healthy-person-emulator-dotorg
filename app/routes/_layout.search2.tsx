feec362274a2d37a52ae920c64c9b5bedb9b25bb
contradiction29 <sora32127@gmail.com>
2025-06-16 21:11:07 +0900

    feat: :sparkles: uRLの取得経路を追加する

--- app/routes/_layout.search2.tsx
+++ app/routes/_layout.search2.tsx
@@ -1,4 +1,33 @@
+import { useLoaderData } from "@remix-run/react";
+
+export async function loader() {
+    const { searchURL, tagsURL } = getObjectUrls();
+    return {
+        searchURL,
+        tagsURL,
+    }
+}
 
 export default function Search2() {
-    return <div>Search2</div>;
+    const { searchURL, tagsURL } = useLoaderData<typeof loader>();
+    return <div>
+        <h1>Search2</h1>
+        <p>{searchURL}</p>
+        <p>{tagsURL}</p>
+    </div>;
+}
+
+function getObjectUrls(): {
+    searchURL: string;
+    tagsURL: string;
+} {
+    const searchURL = process.env.SEARCH_PARQUET_FILE_PATH
+    const tagsURL = process.env.TAGS_PARQEST_FILE_PATH
+    if (!searchURL || !tagsURL) {
+        throw new Error("SEARCH_PARQUET_URL or TAGS_PARQUET_URL is not set");
+    }
+    return {
+        searchURL,
+        tagsURL,
+    }
 }
