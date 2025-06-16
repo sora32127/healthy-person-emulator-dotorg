cbdd3d168476d67eecadde73b4cfb4caf81fc005
contradiction29 <sora32127@gmail.com>
2025-06-16 21:38:40 +0900

    feat: :sparkles: duckdbの初期化関数を実装する

--- app/routes/_layout.search2.tsx
+++ app/routes/_layout.search2.tsx
@@ -1,5 +1,7 @@
 import { useFetcher, useLoaderData } from "@remix-run/react";
 import useSWR from 'swr'
+import * as duckdb from '@duckdb/duckdb-wasm';
+import { useEffect } from "react";
 
 export async function loader() {
     const { searchURL, tagsURL } = getObjectUrls();
@@ -13,6 +15,12 @@ export default function Search2() {
     const { searchURL, tagsURL } = useLoaderData<typeof loader>();
     const { data: searchData, error: searchError, isLoading: searchLoading } = useDownloadFile(searchURL);
     const { data: tagsData, error: tagsError, isLoading: tagsLoading } = useDownloadFile(tagsURL);
+    useEffect(() => {
+        if (!searchLoading && !tagsLoading && searchData && tagsData) {
+            console.log("Data is downloaded");
+            initializeDuckDB(searchData, tagsData);
+        }
+    }, [searchLoading, tagsLoading, searchData, tagsData]);
     return <div>
         {searchLoading && <div>Loading...</div>}
         {searchError && <div>Error: {searchError.message}</div>}
@@ -20,6 +28,38 @@ export default function Search2() {
         {tagsError && <div>Error: {tagsError.message}</div>}
     </div>;
 }
+
+async function initializeDuckDB(searchBlob: Blob, tagsBlob: Blob) {
+    const JSDELIVR_BUNDLES = duckdb.getJsDelivrBundles();
+    const bundle = await duckdb.selectBundle(JSDELIVR_BUNDLES);
+    const worker_url = URL.createObjectURL(
+    new Blob([`importScripts("${bundle.mainWorker!}");`], {type: 'text/javascript'})
+    );
+
+    const worker = new Worker(worker_url);
+    const logger = new duckdb.ConsoleLogger()
+    const db = new duckdb.AsyncDuckDB(logger, worker);
+    await db.instantiate(bundle.mainModule, bundle.pthreadWorker);
+    URL.revokeObjectURL(worker_url);
+    
+    // BlobデータをArrayBufferに変換
+    const tagsArrayBuffer = await tagsBlob.arrayBuffer();
+    const searchArrayBuffer = await searchBlob.arrayBuffer();
+    
+    // ArrayBufferをDuckDBのファイルシステムに登録
+    await db.registerFileBuffer('tags.parquet', new Uint8Array(tagsArrayBuffer));
+    await db.registerFileBuffer('search.parquet', new Uint8Array(searchArrayBuffer));
+    
+    // データの挿入を実施する
+    const conn = await db.connect();
+    await conn.query("INSTALL parquet");
+    await conn.query("LOAD 'parquet'");
+    await conn.query(`CREATE TABLE tags AS SELECT * FROM read_parquet('tags.parquet')`);
+    await conn.query(`CREATE TABLE search AS SELECT * FROM read_parquet('search.parquet')`);
+    await conn.close();
+    return db;
+}
+
 
 function useDownloadFile(url:string) {
     const fetcher = (url: string) => fetch(url).then(res => res.blob());
